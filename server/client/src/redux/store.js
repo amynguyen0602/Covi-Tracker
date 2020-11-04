4d20cbf306e5ae1eebd293e0298b98aa80a4fa29
Linh Nguyen <thuylinh11t@gmail.com>
2020-11-04 11:07:31 -0800

    fix type error redux

--- server/client/src/redux/store.js
+++ server/client/src/redux/store.js
@@ -18,7 +18,10 @@ const reducers = combineReducers({
 const store = createStore(
   reducers,
   initialState,
-  compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+  compose(applyMiddleware(...middleware), typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+  ? a => a
+  : window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__())
 )
 
 export default store

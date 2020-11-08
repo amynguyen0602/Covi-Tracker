0998205561f8f264ec07af7b9e8775bcfa134967
GAHYUN LEE <gahyunlee26@gmail.com>
2020-11-07 18:36:10 -0800

    Map shows on the page

--- server/client/src/components/map/Map.js
+++ server/client/src/components/map/Map.js
@@ -1,9 +1,16 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react'
+import GoogleMapReact from 'google-map-react'
 
-export class Map extends Component {
-	render() {
-		return <div>Map here</div>;
-	}
+function Map() {
+  const [currentGeoLocation, setCurrentGeoLocation] = useState({ lat: 49.246292, lng: -123.116226 })
+
+  useEffect(() => {})
+
+  return (
+    <div style={{ height: '70vh', width: 'calc(100vw - 100px)' }}>
+      <GoogleMapReact defaultCenter={currentGeoLocation} defaultZoom={11}></GoogleMapReact>
+    </div>
+  )
 }
 
-export default Map;
+export default Map

9f136ea1d23eeb0c117caeb613f812e10b9c135e
GAHYUN LEE <gahyunlee26@gmail.com>
2020-11-11 15:29:16 -0800

    Map shows current location 500m radius

--- server/client/src/components/map/Map.js
+++ server/client/src/components/map/Map.js
@@ -1,14 +1,34 @@
 import React, { useEffect, useState } from 'react'
 import GoogleMapReact from 'google-map-react'
+import { Input } from 'antd'
+import { AudioOutlined } from '@ant-design/icons'
+
+const { Search } = Input
 
 function Map() {
   const [currentGeoLocation, setCurrentGeoLocation] = useState({ lat: 49.246292, lng: -123.116226 })
 
-  useEffect(() => {})
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setCurrentGeoLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        })
+      })
+    }
+  }, [])
+
+  const handleSearch = () => {
+    console.log('search button clicked')
+  }
 
   return (
-    <div style={{ height: '70vh', width: 'calc(100vw - 100px)' }}>
-      <GoogleMapReact defaultCenter={currentGeoLocation} defaultZoom={11}></GoogleMapReact>
+    <div style={{ width: '80vw', margin: '0px auto' }}>
+      <Search placeholder="input search text" onSearch={handleSearch} enterButton size="large" />
+      <div style={{ height: '70vh', width: 'calc(100vw - 100px)' }}>
+        <GoogleMapReact defaultCenter={currentGeoLocation} defaultZoom={14}></GoogleMapReact>
+      </div>
     </div>
   )
 }

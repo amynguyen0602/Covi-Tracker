d1b35b0cc1ea0db504ff623abfcc82ad7d72f648
Amy Nguyen <thuylinh11t@gmail.com>
2020-12-03 15:15:07 -0800

    create testing center routes

--- server/routes/testingCentreRoutes.js
+++ server/routes/testingCentreRoutes.js
@@ -1,9 +1,16 @@
 const https = require('https');
 
 module.exports = (app) => {
-    app.get('api/testingcentre', (req, res) => {
+    app.get('/api/testingcentre', (req, res) => {
         https.get('https://services1.arcgis.com/B6yKvIZqzuOr0jBR/arcgis/rest/services/COVID19_Testing_Centres_in_Canada/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json', response => {
-            console.log(response)
+            var data = ''
+            response.on("data", d => {
+                data += d
+            })
+            response.on("end", () => {
+                const testingCenterData = JSON.parse(data)
+                res.send(testingCenterData)
+            })
         })
     })
 }
